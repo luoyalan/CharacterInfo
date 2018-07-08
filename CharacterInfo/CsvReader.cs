1cdf66827a98b872cf63b0bc7f57f5d7c89a13e8
yll <yll@outlook.com>
2018-07-08 22:24:19 +0200

    initial commit

---
+++ CharacterInfo/CsvReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterInfo
+{
+   public class CsvReader
+    {
+        public string[] Readdecomp()
+        {
+
+            List<string> characters = new List<string>();
+            return Properties.Resources.decomp.Split(Environment.NewLine[0]);
+        }
+        public string[] ReaddecompT()
+        {
+
+            List<string> characters = new List<string>();
+            return Properties.Resources.decompT.Split(new[] { "\r\n", "\r", "\n" },StringSplitOptions.None);
+        }
+        public string[] Readadjlist()
+        {
+
+            List<string> characters = new List<string>();
+            return Properties.Resources.AdjList.Split(Environment.NewLine[0]);
+        }
+    }
+}
---
+++ CharacterInfo/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CharacterInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CsvReader csvReader = new CsvReader();
+            if (radioButton1.Checked)
+            {
+                var characters = csvReader.ReaddecompT();
+                display(characters);
+            }
+            else
+            {
+                var characters = csvReader.Readdecomp();
+                display(characters);
+            }
+
+
+        }
+
+
+
+        private void display(string[] characters)
+        {
+            var matchingstring = characters.FirstOrDefault(stringToCheck => stringToCheck.Contains(this.textBox1.Text));
+            int index = 0;
+            index = matchingstring.IndexOf(',');
+            matchingstring = matchingstring.Substring(index + 1);
+            this.label1.Text = matchingstring;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CsvReader csvReader = new CsvReader();
+            var characters = csvReader.Readadjlist();
+            var matchingstring = characters.FirstOrDefault(stringToCheck => stringToCheck.Contains(this.textBox1.Text));
+            int index = 0;
+            index = matchingstring.IndexOf(',');
+            matchingstring = matchingstring.Substring(index + 1);
+            this.label1.Text = matchingstring;
+        }
+    }
+}
